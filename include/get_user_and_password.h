ab3ba57e2ba003834ae235e32d9ea9057a163865
Clement Peau <clement.peau@epitech.eu>
2017-05-16 17:37:49 +0200

    Created get_user_and_password to handle the getting of user and pass comamnds as well as the login

---
+++ include/get_user_and_password.h
@@ -0,0 +1,10 @@
+//
+// Created by peau_c on 5/16/17.
+//
+
+#ifndef MYFTP_GET_USER_AND_PASSWORD_H
+#define MYFTP_GET_USER_AND_PASSWORD_H
+
+int get_user_and_pass(t_server *server, t_client *client);
+
+#endif //MYFTP_GET_USER_AND_PASSWORD_H
---
+++ src/get_user_and_password.c
@@ -0,0 +1,66 @@
+//
+// Created by peau_c on 5/16/17.
+//
+
+#include <string.h>
+#include <stdio.h>
+#include "server.h"
+#include "get_next_command.h"
+#include "get_user_and_password.h"
+
+static t_command *get_user(t_client *client)
+{
+	t_command	*user;
+	
+	dprintf(2, "GetUser\n");
+	if ((user = get_next_command(client->fd)) == FATAL_ERROR
+		|| user == SYNTAX_ERROR)
+		return ((int) user);
+	if (user->instruction == PASS)
+	{
+		send_wrong_login_order(client);
+		return (SYNTAX_ERROR);
+	}
+	else if (user->instruction != USER)
+	{
+		send_wrong_user_pass_response(client);
+		return (SYNTAX_ERROR);
+	}
+	send_input_pass_response(client);
+	return (user);
+}
+
+static t_command *get_pass(t_client *client)
+{
+	t_command	*pass;
+	
+	dprintf(2, "GetPass\n");
+	if ((pass = get_next_command(client->fd)) == FATAL_ERROR
+		|| pass == SYNTAX_ERROR)
+		return ((int) pass);
+	if (pass->instruction != PASS)
+	{
+		send_wrong_user_pass_response(client);
+		return (SYNTAX_ERROR);
+	}
+	return (pass);
+}
+
+static void handle_login(t_client *client, t_command *user, t_command *pass)
+{
+	if (strcmp(user->arguments, "anonymous") == 0 && strcmp(pass->arguments, "") == 0)
+		client->is_loged = true;
+	else
+		send_wrong_login_response(client);
+}
+int get_user_and_pass(t_server *server, t_client *client)
+{
+	t_command *user;
+	t_command *pass;
+	
+	dprintf(2, "getUserAndPass\n");
+	user = get_user(client);
+	pass = get_pass(client);
+	handle_login(client, user, pass);
+	return (0);
+}
